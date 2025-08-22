e75566ccfe1efd22647e4905052eb08247b8bf3c
Yurii <yurii.nazarenko.dev@gmail.com>
2025-08-22 21:31:49 +0300

    Fix react router basename

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router";
 
 export const Providers = ({ children }) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return <BrowserRouter basename="/ecommerce-client">{children}</BrowserRouter>;
 };
